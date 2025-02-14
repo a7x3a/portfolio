e3511198957c46dad5bcaaed5dd1d7f04bfdb5b5
a7x3a <ahmad.07002706@univsul.edu.iq>
2025-02-15 00:28:49 +0300

    initing portfolio v2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-[100dvh] flex justify-center items-center bg-red-400">
+        <p className="text-5xl  text-white font-bold tracking-widest uppercase">START FROM HERE</p>
+      </div>
+    </>
+  )
+}
+
+export default App
