dce44b2329a54f04f854fe9958b336b1b70ea7b0
a7x3a <ahmad.07002706@univsul.edu.iq>
2025-02-15 00:50:36 +0300

    added matrial-tailwind v3 to v4 tailwindcss :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-
+import { Avatar , Button } from "@material-tailwind/react"
 function App() {
 
   return (
     <>
-      <div className="w-full h-[100dvh] flex justify-center items-center bg-red-400">
+      <div className="w-full h-[100dvh] flex flex-col gap-10 justify-center items-center bg-red-400">
         <p className="text-5xl  text-white font-bold tracking-widest uppercase">START FROM HERE</p>
+        <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg" alt="avatar" />
+        <Button color="white" style={{cursor : 'pointer'}} size="regular" ripple="light">Click</Button>
       </div>
     </>
   )
---
+++ src/utils/Button.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@material-tailwind/react"
+function Button({childs}) {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
